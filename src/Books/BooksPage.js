638fdc6965c4f63d82a80948043e9fd31496789f
CSE <revengerreigns076@gmail.com>
2020-09-01 20:51:31 +0600

    Search icon added

--- src/Books/BooksPage.js
+++ src/Books/BooksPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./BooksDesign.css";
 import Books from "./Books";
 import Card from "./Card";
+import SearchIcon from "../images/search2.png";
 
 function BooksPage() {
   useEffect(() => {
@@ -10,6 +11,19 @@ function BooksPage() {
 
   return (
     <div className="container bookdiv ">
+      <div className="searchdiv">
+        <img
+          src={SearchIcon}
+          alt="search"
+          style={{ width: "60px", marginTop: "1rem", marginRight: "0.5rem" }}
+        />
+        <input
+          className="searchInput"
+          placeholder="Search Book"
+          name="search"
+          spellCheck="false"
+        />
+      </div>
       <div className="row justify-content-center text-center">
         {Books.map((item) => {
           return <Card key={item.id} value={item} />;
